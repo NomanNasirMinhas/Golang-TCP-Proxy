a4a026c524a918769902ee2594a2190e9221331c
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2023-11-06 18:27:29 +0500

    liboqs added on mac

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	var isServer bool
 	var isClient bool
 	var useTLS bool
+	var usePQC bool
 	var certFile string
 	var keyFile string
 	var generateCert bool
@@ -17,6 +18,7 @@ func main() {
 	flag.BoolVar(&isServer, "server", false, "Start in server mode")
 	flag.BoolVar(&isClient, "client", false, "Start in client mode")
 	flag.BoolVar(&useTLS, "tls", false, "Use TLS")
+	flag.BoolVar(&usePQC, "pq_tls", false, "Use PQ-TLS")
 	flag.StringVar(&certFile, "cert", "cert.pem", "Certificate file")
 	flag.StringVar(&keyFile, "key", "key.pem", "Key file")
 	flag.BoolVar(&generateCert, "generate-cert", false, "Generate a self-signed certificate")
@@ -26,12 +28,26 @@ func main() {
 		flag.Usage()
 		return
 	}
+	if !isServer && !isClient {
+		panic("Must start in either server or client mode")
+	}
 	if isServer && isClient {
 		panic("Cannot start in both server and client mode")
 	}
+	if usePQC && useTLS {
+		panic("Cannot use both TLS and PQ-TLS")
+	}
+	tls_type := 0
+	if useTLS {
+		tls_type = 1
+	} else if usePQC {
+		tls_type = 2
+	} else {
+		tls_type = 0
+	}
 
 	if isServer {
-		startServer(addr, useTLS, certFile, keyFile, generateCert)
+		startServer(addr, int8(tls_type), certFile, keyFile, generateCert)
 	} else {
 		startClient(addr, useTLS, certFile)
 	}
--- server.go
+++ server.go
@@ -9,10 +9,11 @@ import (
 	"strings"
 )
 
-func startServer(addr string, useTLS bool, certFile string, keyFile string, generateCert bool) {
+func startServer(addr string, TLS_Type int8, certFile string, keyFile string, generateCert bool) {
 	var ln net.Listener
 	var err error
-	if useTLS {
+	if TLS_Type == 1 {
+		println("Running TLS server")
 		// Check if the certificate and key files exist
 		if _, err := os.Stat(certFile); os.IsNotExist(err) {
 			log.Printf("certificate file %s does not exist.", certFile)
@@ -22,35 +23,55 @@ func startServer(addr string, useTLS bool, certFile string, keyFile string, gene
 			} else {
 				log.Fatalf("use -generate-cert to generate a self-signed certificate")
 			}
-			if _, err := os.Stat(keyFile); os.IsNotExist(err) {
-				log.Fatalf("key file %s does not exist", keyFile)
-			}
-			cert, err := tls.LoadX509KeyPair(certFile, keyFile)
-			if err != nil {
-				log.Fatal(err)
-			}
-			config := &tls.Config{Certificates: []tls.Certificate{cert}}
-			log.Printf("listening on port %s\n", addr)
-			ln, err = tls.Listen("tcp", addr, config)
-		} else {
-			ln, err = net.Listen("tcp", addr)
 		}
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		config := &tls.Config{Certificates: []tls.Certificate{cert}}
+		log.Printf("listening on port %s\n", addr)
+		ln, err = tls.Listen("tcp", addr, config)
+
+	} else if TLS_Type == 2 {
+		println("Running PQ-TLS server")
+		// Check if the certificate and key files exist
+		if _, err := os.Stat(certFile); os.IsNotExist(err) {
+			log.Printf("certificate file %s does not exist.", certFile)
+			if generateCert {
+				log.Printf("generating self-signed certificate %s", certFile)
+				generateCertFunc(strings.Split(addr, ":")[0])
+			} else {
+				log.Fatalf("use -generate-cert to generate a self-signed certificate")
+			}
+		}
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		config := &tls.Config{Certificates: []tls.Certificate{cert}}
+		log.Printf("listening on port %s\n", addr)
+		ln, err = tls.Listen("tcp", addr, config)
+
+	} else {
+		println("Running plaintext server")
+		ln, err = net.Listen("tcp", addr)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Accept incoming connections and handle them
+	for {
+		conn, err := ln.Accept()
 		if err != nil {
 			fmt.Println(err)
-			return
+			continue
 		}
 
-		// Accept incoming connections and handle them
-		for {
-			conn, err := ln.Accept()
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-
-			// Handle the connection in a new goroutine
-			go handleConnection(conn)
-		}
+		// Handle the connection in a new goroutine
+		go handleConnection(conn)
 	}
 }
 
